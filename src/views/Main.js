99d8e557d51476ca58d102e253ba2d11b8b18fd5
ParkMi-Hyun <parkmihyunn@gmail.com>
2025-01-29 20:33:26 +0900

    키워드검색

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { useEffect, useState, useMemo } from "react";
-import { Map as KakaoMap, MapMarker, Circle } from "react-kakao-maps-sdk";
+import { Map, MapMarker, Circle } from "react-kakao-maps-sdk";
 import { ReactComponent as IconMyLocation } from "../assets/acc-icon.svg";
 import { debounce } from "lodash";
 
+const { kakao } = window;
+
 export default function Main() {
   // 지도의 중심좌표
+
   const [center, setCenter] = useState({
     lat: 37.503081,
     lng: 127.04158,
@@ -46,15 +49,60 @@ export default function Main() {
     });
   }, []);
 
+  // 키워드 검색
+  const { kakao } = window;
+  const [info, setInfo] = useState();
+  const [markers, setMarkers] = useState([]);
+  const [map, setMap] = useState();
+
+  useEffect(() => {
+    if (!map) return;
+    const ps = new kakao.maps.services.Places();
+
+    ps.keywordSearch("금천구 맛집", (data, status, _pagination) => {
+      if (status === kakao.maps.services.Status.OK) {
+        let markers = [];
+
+        for (var i = 0; i < data.length; i++) {
+          // @ts-ignore
+          markers.push({
+            position: {
+              lat: data[i].y,
+              lng: data[i].x,
+            },
+            content: data[i].place_name,
+          });
+        }
+        setMarkers(markers);
+      }
+    });
+  }, [map]);
+
   return (
     <div className="relative w-full h-full">
-      <KakaoMap // 지도를 표시할 Container
+      <Map
         className="w-full h-full"
         id="map"
         center={center}
-        level={3} // 지도의 확대 레벨
+        level={3}
         onCenterChanged={updateCenterWhenMapMoved}
+        onCreate={setMap}
       >
+        {markers.map((marker) => (
+          <MapMarker
+            key={`marker-${marker.content}-${marker.position.lat},${marker.position.lng}`}
+            position={marker.position}
+            onClick={() => setInfo(marker)}
+            image={{
+              src: require("../assets/map-marker.svg").default,
+              size: { width: 30, height: 30 },
+            }}
+          >
+            {info && info.content === marker.content && (
+              <div style={{ color: "#000" }}>{marker.content}</div>
+            )}
+          </MapMarker>
+        ))}
         <MapMarker
           image={{
             src: require("../assets/map-pin.svg").default,
@@ -65,12 +113,14 @@ export default function Main() {
         <Circle
           center={position}
           radius={2000}
-          strokeWeight={0} // 선의 두께입니다
+          strokeWeight={0}
           strokeOpacity={0}
-          fillColor={"#b2e39d"} // 채우기 색깔입니다
-          fillOpacity={0.5} // 채우기 불투명도 입니다
+          fillColor={"#b2e39d"}
+          fillOpacity={0.5}
         />
-      </KakaoMap>
+      </Map>
+
+      {/* 현위치 이동 버튼 */}
       <div className="flex flex-col gap-[10px] absolute z-[1] top-0 right-0 p-[10px]">
         <button
           className="flex justify-center items-center cursor-pointer rounded-full w-[45px] h-[45px] bg-white shadow-[0_0_8px_#00000025]"
