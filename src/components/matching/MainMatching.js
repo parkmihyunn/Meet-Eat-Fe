d2a6a5db8014473028e3a7e39a8467238c4729d7
ParkMi-Hyun <parkmihyunn@gmail.com>
2025-01-31 18:31:31 +0900

    Feat: 범위 내 키워드 검색

--- src/components/matching/MainMatching.js
+++ src/components/matching/MainMatching.js
@@ -3,6 +3,8 @@ import { useEffect, useState, useMemo } from "react";
 import { Map, MapMarker, Circle } from "react-kakao-maps-sdk";
 import { ReactComponent as IconMyLocation } from "../../assets/acc-icon.svg";
 import { debounce } from "lodash";
+import SearchBar from "./SearchBar";
+import axios from "axios";
 
 export default function Main() {
   // 지도의 중심좌표
@@ -19,7 +21,6 @@ export default function Main() {
 
   // 지도의 중심을 유저의 현재 위치로 변경
   const setCenterToMyPosition = () => {
-    console.log("클릭");
     setCenter(position);
   };
 
@@ -46,85 +47,97 @@ export default function Main() {
     });
   }, []);
 
-  // 키워드 검색
+  // 2000m이내 키워드 검색
   const { kakao } = window;
   const [info, setInfo] = useState();
   const [markers, setMarkers] = useState([]);
-  const [map, setMap] = useState();
 
   useEffect(() => {
-    if (!map) return;
-    const ps = new kakao.maps.services.Places();
-
-    ps.keywordSearch("금천구 맛집", (data, status, _pagination) => {
-      if (status === kakao.maps.services.Status.OK) {
+    axios
+      .get(`https://dapi.kakao.com/v2/local/search/keyword?`, {
+        params: {
+          x: position.lng,
+          y: position.lat,
+          radius: 200,
+          category_group_code: "FD6",
+          query: "총각네 부추곱창",
+        },
+        headers: {
+          Authorization: `KakaoAK ${process.env.REACT_APP_RESTAPI_KEY}`,
+        },
+      })
+      .then((res) => {
         let markers = [];
-
-        for (var i = 0; i < data.length; i++) {
-          // @ts-ignore
+        for (let i = 0; i < res.data.documents.length; i++) {
           markers.push({
             position: {
-              lat: data[i].y,
-              lng: data[i].x,
+              lat: res.data.documents[i].y,
+              lng: res.data.documents[i].x,
             },
-            content: data[i].place_name,
+            place_name: res.data.documents[i].place_name,
+            category_name: res.data.documents[i].category_name,
+            id: res.data.documents[i].id,
+            phone: res.data.documents[i].phone,
+            place_url: res.data.documents[i].place_url,
+            road_address_name: res.data.documents[i].road_address_name,
           });
         }
         setMarkers(markers);
-      }
-    });
-  }, [map]);
+      });
+  }, [position]);
 
   return (
-    <div className="relative w-full h-full">
-      <Map
-        className="w-full h-full"
-        id="map"
-        center={center}
-        level={3}
-        onCenterChanged={updateCenterWhenMapMoved}
-        onCreate={setMap}
-      >
-        {markers.map((marker) => (
+    <div className="relative">
+      <div className="relative w-full h-full">
+        <Map
+          className="w-full h-full"
+          id="map"
+          center={center}
+          level={5}
+          onCenterChanged={updateCenterWhenMapMoved}
+        >
+          {markers.map((marker) => (
+            <MapMarker
+              key={`marker-${marker.content}-${marker.position.lat},${marker.position.lng}`}
+              position={marker.position}
+              onClick={() => setInfo(marker)}
+              image={{
+                src: require("../../assets/map-marker.svg").default,
+                size: { width: 30, height: 30 },
+              }}
+            >
+              {info && info.place_name === marker.place_name && (
+                <div>{marker.place_name}</div>
+              )}
+            </MapMarker>
+          ))}
           <MapMarker
-            key={`marker-${marker.content}-${marker.position.lat},${marker.position.lng}`}
-            position={marker.position}
-            onClick={() => setInfo(marker)}
             image={{
-              src: require("../../assets/map-marker.svg").default,
+              src: require("../../assets/map-pin.svg").default,
               size: { width: 30, height: 30 },
             }}
-          >
-            {info && info.content === marker.content && (
-              <div style={{ color: "#000" }}>{marker.content}</div>
-            )}
-          </MapMarker>
-        ))}
-        <MapMarker
-          image={{
-            src: require("../../assets/map-pin.svg").default,
-            size: { width: 30, height: 30 },
-          }}
-          position={position}
-        />
-        <Circle
-          center={position}
-          radius={2000}
-          strokeWeight={0}
-          strokeOpacity={0}
-          fillColor={"#b2e39d"}
-          fillOpacity={0.5}
-        />
-      </Map>
+            position={position}
+          />
+          <Circle
+            center={position}
+            radius={2000}
+            strokeWeight={0}
+            strokeOpacity={0}
+            fillColor={"#b2e39d"}
+            fillOpacity={0.5}
+          />
+        </Map>
 
-      {/* 현위치 이동 버튼 */}
-      <div className="flex flex-col gap-[10px] absolute z-[1] top-0 right-0 p-[10px]">
-        <button
-          className="flex justify-center items-center cursor-pointer rounded-full w-[45px] h-[45px] bg-white shadow-[0_0_8px_#00000025]"
-          onClick={setCenterToMyPosition}
-        >
-          <IconMyLocation width="25px" />
-        </button>
+        {/* 현위치로 지도도 이동 버튼 */}
+        <div className="flex flex-col gap-[10px] absolute z-[1] top-0 right-0 p-[10px]">
+          <button
+            className="flex justify-center items-center cursor-pointer rounded-full w-[45px] h-[45px] bg-white shadow-[0_0_8px_#00000025]"
+            onClick={setCenterToMyPosition}
+          >
+            <IconMyLocation width="25px" />
+          </button>
+        </div>
+        <SearchBar />
       </div>
     </div>
   );
---
+++ src/components/matching/SearchBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function SearchBar() {
+  return (
+    <div className="flex flex-row absolute z-[1] bottom-20 w-1/2 h-10 bg-emerald-600 translate-x-[-50%] left-[50%]">
+      <input />
+      <button></button>
+    </div>
+  );
+}
